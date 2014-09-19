03ad0f802a980f0ce14b75978ab3bb309e614e76
Guido Grassel <guido@Guidos-MacBook-Pro.local>
2014-09-19 18:00:02 +0300

    see previous commit, wy does Xcode not know about amending ...

--- SwiftRSS/RssFeedChannelModel.swift
+++ SwiftRSS/RssFeedChannelModel.swift
@@ -7,5 +7,11 @@ class RssFeedChannelModel : NSManagedObject {
     @NSManaged var urlString : String;
     @NSManaged var titleString : String;
 
+    /* see https://developer.apple.com/library/prerelease/mac/documentation/Swift/Conceptual/BuildingCocoaApps/WritingSwiftClassesWithObjective-CBehavior.html
+    section : Implementing Core Data Managed Object Subclasses
+    
+    Swift classes are namespaced—they’re scoped to the module (typically, the project) they are compiled in. To use a Swift subclass of the NSManagedObject class with your Core Data model, prefix the class name in the Class field in the model entity inspector with the name of your module.
+    */
+
 }
 
